de603789528553a9bfa50359215ad540290e0cf3
Debra Ray <raydebra89@gmail.com>
2019-11-23 20:09:35 -0500

    Convert String to Datetime

--- datetime_exercise.py
+++ datetime_exercise.py
@@ -125,5 +125,91 @@ A function to return the speed of a function. This is getting meta.
 def speed_tester(function, s):
     ''' 
     returns the time required to convert the series `s` using the function `function`
-        note: this is a bad function and is not     
+        note: this is a bad function and is not efficient for measuring speed in scale.
+    '''
     
+    before = datetime.datetime.now()
+    s = function(s)
+    after = datetime.datetime.now()
+    
+    seconds = (after - before).seconds
+    microseconds = (after - before).microseconds
+    speed = int(seconds) + float(microseconds/10**5)
+    return speed
+
+'''
+Several datetime conversion methods
+Which method will turn out the best?
+'''
+
+def f_pandas(s):
+    ''' 
+    converts from string format to dt format using pd.to_datetime()
+    '''
+    
+    return pd.to_datetime(s)
+
+def f_datetime(s):
+    '''
+    converts from string format to dt format
+    Each item in the series is converted with this datetime using the apply method of the pandas series without
+    automated string format detection.
+    '''
+    return s.apply(lambda x:datetime.datetime.strptime(x, '%b-%d-%Y'))
+
+def f_memoize_pandas(s):
+    ''' 
+    Memoization technique
+    A dictionary of unique dates is instantiated with keys as strings and values as their datetime format
+    The dictionary is then mapped to the pandas series. 
+    This technique dramatically increases performance by avoiding repeated dates. 
+    Plus: Automated string format detection!!
+    '''
+    dates = {date: pd.to_datetime(date) for date in s.unique()}
+    return s.map(dates)        
+
+def f_memoize_dt(s):
+    '''
+    Memoization technique
+    Same memoization technique as above except using a datetime parser instead of pandas.
+    Minus: No Automated string format detection 
+    '''
+    dates = {date:datetime.datetime.strptime(date, '%b-%d-%Y') for date in s.unique()}
+    return s.map(dates)
+
+'''
+Pandas functions are slower most likely due to the automated string format detection. 
+Fastest memoized datetime function is about 486x faster than the standard pandas function.
+Underlying Cython of Pandas increases performance of Pandas after n unique ** 3 to other methods. 
+f_datetime fastest out of all of them!
+'''
+
+# Add random amount of seconds to each date
+# This puts more stress on the parsing process and will be more computationally taxing. 
+
+# I did not perform a full simulation in this scenario given the extreme time for computation
+# datetime parsers don't work with messy data since they require a specified parsing format
+
+#10^3 Unique dates 
+#10^6 Overall
+
+{'Function' : 'f_pandas', 'speed': 160.412103}
+{'Function': 'f_memoize_pd', 'speed':.495355}
+
+#10^3 Unique Dates
+#10^7 Overall
+{'Function': 'f_pandas', 'speed':1421.018452}
+{'Function': 'f_memoize_pd', 'speed':1.61473}
+
+#With 10^3 Unique Dates (10^6 Overall): Memoized version is 325x faster than standard
+#With 10^3 Unique Dates (10^7 Overall): memoized version is 880x faster than standard
+
+'''
+Conclusion:
+Magnitude of improvement should be expected to increase in relation to task complexity with the methods provided
+If your dates contain repeats, you should consider a memoization approach. 
+If your dates do not contain repeats (or contain fewer than 100 repeats), you should use the built in pandas method. 
+
+While pandas is super quick for most data conversions, it can be extremely slow for converting from string to datetime format. 
+An alternative memoized approach is given and compared to standard methods, performing up to 480x faster (880x with noisy data).
+'''
