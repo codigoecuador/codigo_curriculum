00a4f679637c77fb62adcd834c5456c92be871a7
Debra Ray <raydebra89@gmail.com>
2020-01-18 18:43:08 -0500

    Update scrape_dividend_cal_tutorial.py

--- Python Advanced_webscraping/scrape_dividend_cal_tutorial.py
+++ Python Advanced_webscraping/scrape_dividend_cal_tutorial.py
@@ -5,12 +5,19 @@ Created on Fri Jan 17 16:53:24 2020
 @author: rayde
 """
 
-import pandas, json, requests, datetime, calendar
+import pandas, requests, datetime, calendar
 
 class dividend_calendar:
-     calendars = []
-     errors = []
-     def __init__(self, year, month):
+    #class attributes 
+    calendars = [] 
+    url = 'https://api.nasdaq.com/api/calendar/dividends'
+    hdrs =  {'Accept': 'application/json, text/plain, */*',
+                 'DNT': "1",
+                 'Origin': 'https://www.nasdaq.com/',
+                 'Sec-Fetch-Mode': 'cors',
+                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0)'}
+
+    def __init__(self, year, month):
           '''
         Parameters
         ----------
@@ -19,100 +26,53 @@ class dividend_calendar:
         
         Returns
         -------
-        Sets instance attributes for year, month, and day of object.
-        Class attributes are calendars and methods.
+        Sets instance attributes for year and month of object.
+        Empty calendars list is a class attribute.
+        
           '''
+          #instance attributes
           self.year = int(year)
-          self.month = int(month)  
-     
-     
-     def url(self, date):
-         '''
-
-         Parameters
-         ----------
-         date : date str
-
-         Returns
-         -------
-         URL = base + subdomain
-             technically when we use requests we can simply pass these three parameters in as a 
-             dictionary with params = {'year':year, 'month':month, 'day':day}
-             For this example we will pass the paramaters manually into the URL.
-
-         '''
-         base = 'https://api.nasdaq.com/api/'
-         subdomain = "calendar/dividends?date={0}"
-         subdomain = subdomain.format(date)
-         return base + subdomain
+          self.month = int(month)
       
-     def header(self, date):
-         '''
-         Parameters
-         ----------
-         year : year int
-         month : month int
-         day : day int
-
-         Returns
-         -------
-         hdrs : dictionary object containing values to header parameters.
-
-         '''
-         base = 'https://www.nasdaq.com/'
-         referer = "market-activity/dividends?date={0}"
-         hdrs = {'Accept': 'application/json, text/plain, */*',
-                 'DNT': "1",
-                 'Origin': base,
-                 'Referer': base + referer.format(date),
-                 'Sec-Fetch-Mode': 'cors',
-                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0)'}
-         return hdrs
-      
-     def scraper(self, url, hdrs):
+    def scraper(self, date):
          '''
            Parameters
            - - - - - 
            url : URL string
            hdrs : Header information
+           date: 
+               
            Returns
            - - - -
            dictionary : Returns a JSON dictionary at a given URL.
          '''
-         page = requests.get(url, params = hdrs)
-         page = page.content
-         dictionary = json.loads(page)
+         page = requests.get(self.url, headers=self.hdrs, params={'date': date})
+         dictionary = page.json()
          return dictionary
      
-      
-     def dict_to_df(self, dicti):
-          '''
-          Parameters
-          ----------
-          dicti : Output from the scraper method as input.
-          Returns
-          -------
-          calendar : Dataframe of stocks with that exdividend date
-          Appends the dataframe to one of the class attributes
-          If the date is a trading day, it will append a dataframe 
-          to the calendars list (class attribute).
-          If the date provided is a nontrading day, then the method 
-          will append an error message with 'bad or no parameter 
-          [date]' to the errors list (class attribute).
+    
+    def dict_to_df(self, dicti):
          '''
-          try:
-               rows = dicti.get('data').get('calendar').get('rows')
-               calendar = pandas.DataFrame(rows)
-               self.calendars.append(calendar)
-          except:
-               rows = dicti.get('status').get('bCodeMessage')
-               calendar = pandas.DataFrame(rows)
-               self.errors.append(calendar)
-          finally:
-               return calendar
-           
+         Parameters
+         ----------
+         dicti : Output from the scraper method as input.
+         
+         Returns
+         -------
+         calendar : Dataframe of stocks with that exdividend date
+         Appends the dataframe to one of the class attributes
+         
+         If the date is formatted, it will append a dataframe 
+         to the calendars list (class attribute). Otherwise, it will 
+         return an empty dataframe.         
+         '''
+         rows = dicti.get('data').get('calendar').get('rows')
+         calendar = pandas.DataFrame(rows)
+         self.calendars.append(calendar)
+         return calendar
+   
             
-     def calendar(self, day):
+    def calendar(self, day):
           '''
           Combines the above methods into one method.
           
@@ -134,13 +94,10 @@ class dividend_calendar:
           '''
           self.day = day
           
-          date = datetime.date(year, month, day)
-          date = date.strftime(format='%Y-%m-%d')
+          date_obj = datetime.date(self.year, self.month, self.day)
+          date_str = date_obj.strftime(format='%Y-%m-%d')      
           
-          hdrs = self.header(date)
-          url = self.url(date)
-          
-          dictionary = self.scraper(url, hdrs)
+          dictionary = self.scraper(date_str)
           self.dict_to_df(dictionary)
           
           return dictionary
@@ -161,10 +118,10 @@ if __name__ == '__main__':
     #define iterator as a list of integers between 1 and the number of days in the month
     iterator = list(range(1,days_in_month+1))
 
-     #Scrape calendar for each day of the month                    
+    #Scrape calendar for each day of the month                    
     objects = list(map(function, iterator))
 
-     #concatenate all the calendars in the class attribute
+    #concatenate all the calendars in the class attribute
     df = pandas.concat(february.calendars)
     
     #Drop any rows with missing data
@@ -173,26 +130,27 @@ if __name__ == '__main__':
     #set the dataframe's row index to the company name
     df = df.set_index('companyName')
     
+    '''    
     date = datetime.date(year, month, day=28)
-    date = date.strftime(format='%Y-%m-%d')
+    date = date.strftime(format='%Y-%m-%d')    
     
-    urls = february.url(date)
-    urls
+    api_params = {'date': date}
+    hdrs =  {'Accept': 'application/json, text/plain, */*',
+                 'DNT': "1",
+                 'Origin': 'https://www.nasdaq.com/',
+                 'Sec-Fetch-Mode': 'cors',
+                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0)'}
     
-    hdrs = february.header(date)
-    hdrs
-    
-    page = requests.get(urls, params = hdrs)
-    page
-    
-    page = page.content
-    page 
-    
-    dictionary = json.loads(page)
-    dictionary
+    urls = 'https://api.nasdaq.com/api/calendar/dividends'
+    page = requests.get(urls, headers=hdrs, params=api_params)
+    dictionary = page.json()
     
     rows = dictionary.get('data').get('calendar').get('rows')
     rows 
     
     cal = pandas.DataFrame(rows)
     cal
+
+    df = cal.dropna(how='any')    
+    df = df.set_index('companyName')
+    '''
